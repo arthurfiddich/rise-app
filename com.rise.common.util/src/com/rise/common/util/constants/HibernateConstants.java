a3e59961f7495455980489a9c8f78347ae5559f1
arthurfiddich@gmail.com <arthurfiddich@gmail.com@e8a6c82c-ae2d-16eb-dbd3-f86abf8bcd74>
2013-05-22 13:19:27 +0000

    Added full CRUD operations for one entity...

--- com.rise.common.util/src/com/rise/common/util/constants/HibernateConstants.java
+++ com.rise.common.util/src/com/rise/common/util/constants/HibernateConstants.java
@@ -2,9 +2,51 @@ package com.rise.common.util.constants;
 
 public class HibernateConstants {
 
-	public static String HIBERNATE_CFG_XML = "hibernate.cfg.xml";
+	public static final String HIBERNATE_CFG_XML = "hibernate.cfg.xml";
 
-	public static String SELECT_QUERY = "select m from ";
+	public static final String SELECT_QUERY = "select m from ";
 
+	public static final String PATH_VARIABLE_ID = "/{argId}";
+	
+	public static final String RE_DIRECT = "redirect:";
+	
+	public static final String EDIT_MODE = "editMode";
+	
+	public static final String VIEW_SLASH = "/view/";
+	
+	public static final String CREATE = "/create";
+	
+	public static final String UPDATE = "/update";
+	
+	public static final String DELETE = "/delete";
+	
 	public static final String ALIAS_NAME = " m";
+	
+	public static final String ERROR = "error";
+	
+	public static final String LIST = "/list";
+
+	public static final String EDIT = "/edit";
+
+	public static final String VIEW = "/view";
+
+	public static final String SAVE = "/save";
+	
+	public static final String NEW = "/new";
+	
+	public static final String SLASH = "/";
+	
+	// Entity Name Constants
+	public static final String ADDRESS = "address";
+	
+	public static final String ADDRESSES = "addresses";
+
+	public static final String EDIT_ADDRESS = "editAddress";
+	
+	public static final String PATH_VARIABLE_ADDRESS_ID = "/{argAddressId}";
+
+
+
+
+
 }
