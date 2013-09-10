014223f0b5a91e31d2e9fc4ace0654f864a3b7ef
arthurfiddich@gmail.com <arthurfiddich@gmail.com@e8a6c82c-ae2d-16eb-dbd3-f86abf8bcd74>
2013-09-10 07:49:56 +0000

    Preparing ExcelDataImporter class to export the records into an excel. Also Added apache-poi jars.

--- com.rise.common.util/src/com/rise/common/util/database/exporter/ExcelDataExporter.java
+++ com.rise.common.util/src/com/rise/common/util/database/exporter/ExcelDataExporter.java
@@ -19,7 +19,13 @@ import java.util.List;
 import java.util.Map;
 
 import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.ConditionalFormattingRule;
+import org.apache.poi.ss.usermodel.IndexedColors;
+import org.apache.poi.ss.usermodel.PatternFormatting;
 import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.SheetConditionalFormatting;
+import org.apache.poi.ss.util.CellRangeAddress;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
@@ -205,8 +211,28 @@ public class ExcelDataExporter extends TableDataExporter implements
 						}
 					}
 				}
+				shadeAlt(argXssfSheet);
 			}
 		}
+	}
+
+	private void shadeAlt(Sheet sheet) {
+		SheetConditionalFormatting sheetCF = sheet
+				.getSheetConditionalFormatting();
+
+		// Condition 1: Formula Is =A2=A1 (White Font)
+		ConditionalFormattingRule rule1 = sheetCF
+				.createConditionalFormattingRule("MOD(ROW(),5)");
+		PatternFormatting fill1 = rule1.createPatternFormatting();
+		fill1.setFillBackgroundColor(IndexedColors.LIGHT_GREEN.index);
+		fill1.setFillPattern(PatternFormatting.SOLID_FOREGROUND);
+
+		CellRangeAddress[] regions = { CellRangeAddress.valueOf("A1:Z100") };
+
+		sheetCF.addConditionalFormatting(regions, rule1);
+
+		// sheet.createRow(0).createCell(1).setCellValue("Shade Alternating Rows");
+		// sheet.createRow(1).createCell(1).setCellValue("Condition: Formula Is  =MOD(ROW(),2)   (Light Green Fill)");
 	}
 
 	private void exportReferencedData(List<String> argColumnNamesList,
