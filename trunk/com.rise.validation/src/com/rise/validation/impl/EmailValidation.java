ee60e1267a738932c8ef27aa53d554df31177496
arthurfiddich@gmail.com <arthurfiddich@gmail.com@e8a6c82c-ae2d-16eb-dbd3-f86abf8bcd74>
2013-09-27 14:56:54 +0000

    Email Validation...

--- trunk/com.rise.validation/src/com/rise/validation/impl/EmailValidation.java
+++ trunk/com.rise.validation/src/com/rise/validation/impl/EmailValidation.java
@@ -1,17 +1,22 @@
 package com.rise.validation.impl;
 
+import org.apache.commons.validator.routines.EmailValidator;
+
+import com.rise.common.util.checker.Precondition;
 import com.rise.validation.Validation;
 
-public class EmailValidation implements Validation<String, String>{
+public class EmailValidation implements Validation<String, String> {
 
 	@Override
 	public boolean validate(String argToken) throws Exception {
-		return true;
+		if (Precondition.checkNotEmpty(argToken)) {
+			return EmailValidator.getInstance(true).isValid(argToken);
+		}
+		return false;
 	}
 
 	@Override
 	public boolean validate(String argToken, String argRegion) throws Exception {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
