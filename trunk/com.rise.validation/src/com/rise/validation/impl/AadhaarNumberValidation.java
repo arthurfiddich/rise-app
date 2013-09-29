8a55a824a797f3bbec416ec95bec53c8c483a0fc
arthurfiddich@gmail.com <arthurfiddich@gmail.com@e8a6c82c-ae2d-16eb-dbd3-f86abf8bcd74>
2013-09-29 07:33:28 +0000

    Added comments...

--- trunk/com.rise.validation/src/com/rise/validation/impl/AadhaarNumberValidation.java
+++ trunk/com.rise.validation/src/com/rise/validation/impl/AadhaarNumberValidation.java
@@ -2,6 +2,12 @@ package com.rise.validation.impl;
 
 import com.rise.validation.Validation;
 
+/*
+ * Reference Links: http://developer.uidai.gov.in/site/book/export/html/18
+ * http://tahaz.files.wordpress.com/2010/07/aadhaar_authentication_api_1-0.pdf
+ * http://developer.uidai.gov.in/site/downloads
+ */
+
 public class AadhaarNumberValidation implements Validation<String, String>{
 
 	@Override
