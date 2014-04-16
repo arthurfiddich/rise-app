a2dde92ea9cc2385b35ba7c83203c99e9aab2376
arthurfiddich@gmail.com <arthurfiddich@gmail.com@e8a6c82c-ae2d-16eb-dbd3-f86abf8bcd74>
2014-04-16 12:36:52 +0000

    Added continent fetcher class and moved some classes from one package to another package...

--- com.data.generator.google.place.api/test/com/data/generator/geo/names/api/test/ContinentFetcherTest.java
+++ com.data.generator.google.place.api/test/com/data/generator/geo/names/api/test/ContinentFetcherTest.java
@@ -3,9 +3,11 @@ package com.data.generator.geo.names.api.test;
 import com.data.generator.geo.names.api.Fetcher;
 import com.data.generator.geo.names.api.impl.ContinentFetcher;
 import com.data.generator.google.place.api.http.HttpClientImpl;
+import com.data.generator.helper.TenantConfigHelper;
 
 public class ContinentFetcherTest {
 	public static void main(String[] args) {
+		TenantConfigHelper.getInstance();
 		HttpClientImpl.getInstance();
 		Fetcher fetcher = new ContinentFetcher();
 		fetcher.fetch();
