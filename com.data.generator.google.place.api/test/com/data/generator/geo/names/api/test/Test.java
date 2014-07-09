0f9d9d74761a3e8a7d259f21b641d49bb10c1e36
arthurfiddich@gmail.com <arthurfiddich@gmail.com@e8a6c82c-ae2d-16eb-dbd3-f86abf8bcd74>
2014-07-09 12:21:57 +0000

    Added different annotations...

--- com.data.generator.google.place.api/test/com/data/generator/geo/names/api/test/Test.java
+++ com.data.generator.google.place.api/test/com/data/generator/geo/names/api/test/Test.java
@@ -1,6 +1,35 @@
 package com.data.generator.geo.names.api.test;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class Test {
+	private Queue<String> queue = new LinkedList<String>();
+
+	private Test(Queue<String> argQueue) {
+		super();
+		this.queue = argQueue;
+	}
+
+	public void execute(String argUserName) {
+		System.out.println(argUserName);
+		String userName = argUserName;
+		if (userName.equals("Amar3")) {
+			System.out.println("Found");
+		}
+		for (int i = 0; i < 2; i++) {
+			userName = queue.poll();
+			execute(userName);
+		}
+	}
+
 	public static void main(String[] args) {
+		Queue<String> queue = new LinkedList<String>();
+		queue.add("Amar");
+		queue.add("Amar1");
+		queue.add("Amar2");
+		queue.add("Amar3");
+		Test test = new Test(queue);
+		test.execute(test.queue.peek());
 	}
 }
