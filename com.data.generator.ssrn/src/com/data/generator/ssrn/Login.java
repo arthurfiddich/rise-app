d5af22af4ac0c6627fdf8e8555aa266abadbf9c1
arthurfiddich@gmail.com <arthurfiddich@gmail.com@e8a6c82c-ae2d-16eb-dbd3-f86abf8bcd74>
2014-09-06 05:40:30 +0000

    Added a new project that deals the ssrn website...

--- com.data.generator.ssrn/src/com/data/generator/ssrn/Login.java
+++ com.data.generator.ssrn/src/com/data/generator/ssrn/Login.java
@@ -1,6 +1,81 @@
 package com.data.generator.ssrn;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 public class Login {
 
-	
+	public WebDriver driver = new FirefoxDriver();
+
+	public void openTestSite() {
+		driver.navigate().to("https://hq.ssrn.com/login/pubSignInJoin.cfm?");
+	}
+
+	/**
+	 * 
+	 * @param username
+	 * @param Password
+	 * 
+	 *            Logins into the website, by entering provided username and
+	 *            password
+	 */
+	public void login(String username, String Password) {
+
+		WebElement userName_editbox = driver.findElement(By.id("txtLogin"));
+		WebElement password_editbox = driver.findElement(By.id("txtPassword"));
+		WebElement submit_button = driver.findElement(By.id("process"));
+
+		userName_editbox.sendKeys(username);
+		password_editbox.sendKeys(Password);
+		submit_button.click();
+
+	}
+
+	/**
+	 * grabs the status text and saves that into status.txt file
+	 * 
+	 * @throws IOException
+	 */
+	public void getText() throws IOException {
+		String text = driver
+				.findElement(By.xpath("//div[@id='case_login']/h3")).getText();
+		Writer writer = new BufferedWriter(new OutputStreamWriter(
+				new FileOutputStream("status.txt"), "utf-8"));
+		writer.write(text);
+		writer.close();
+
+	}
+
+	/**
+	 * Saves the screenshot
+	 * 
+	 * @throws IOException
+	 */
+	public void saveScreenshot() throws IOException {
+		File scrFile = ((TakesScreenshot) driver)
+				.getScreenshotAs(OutputType.FILE);
+		FileUtils.copyFile(scrFile, new File("screenshot.png"));
+	}
+
+	public void closeBrowser() {
+		driver.close();
+	}
+
+	public static void main(String[] args) {
+		Login login = new Login();
+		login.openTestSite();
+		login.login("shivaprasad.amar@gmail.com", "amarshiva93");
+	}
 }
